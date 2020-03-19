1be7d8a3930ea0fc0e41c53e138817bf60970a98
COAB1 <carol.o.ag@gmail.com>
2020-03-19 19:34:04 -0300

    Create URI 1044 - multiples

---
+++ URI 1044.py
@@ -0,0 +1,23 @@
+'''
+Read two integer values (A and B). After, the program should print the message "Sao Multiplos"
+(are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+
+Input
+The input has two integer numbers.
+
+Output
+Print the relative message to the input as stated above.
+'''
+
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+numbers = [a, b]
+numbers.sort()
+
+if numbers[1] % numbers[0] == 0:
+    print ("Sao Multiplos")
+else:
+    print("Nao sao Multiplos")
