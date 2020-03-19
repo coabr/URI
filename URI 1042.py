515b00f03529270596a08e6161018993cf39fb10
COAB1 <carol.o.ag@gmail.com>
2020-03-19 18:29:50 -0300

    Create UR1 1042 witth sort function

---
+++ URI 1042.py
@@ -0,0 +1,17 @@
+n1, n2, n3 = input().split()
+
+n1 = int(n1) 
+n2 = int(n2)
+n3 = int(n3)
+
+numbers = [n1, n2, n3]
+numbers.sort()
+
+print (numbers[0])
+print (numbers[1])
+print (numbers[2])
+print("")
+
+print (n1)
+print (n2)
+print (n3)
